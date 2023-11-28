b9ef79537cc8a6ca7a8831b00e9ebd020d19d984
Marcelo Bartholomeu <83658457+GHOST54M@users.noreply.github.com>
2023-11-28 14:18:01 -0300

    Update inserirDadosCandidato.php

--- inserirDadosCandidato.php
+++ inserirDadosCandidato.php
@@ -2,21 +2,29 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include ("cnx.php");
-    if ($cxn->connect_error) {
-        die("Falha na conexão: " . $cxn->connect_error);
-    }
     $nome = $_POST['nome'];
     $cpf = $_POST['cpf'];
     $email = $_POST['email'];
-    $saladeprova = $_POST['saladeprova'];  
+    $saladeprova = $_POST['saladeprova'];
+    
+    $selecionar=mysqli_query($cxn, "select * from saladeprova where idsala='$saladeprova'");
+    while ($montar=mysqli_fetch_array($selecionar))
+    {
+        $quantcandidatos = $montar['quantcandidatos'];
+    }
+    echo "$quantcandidatos";
+    if($quantcandidatos < 50 )
+    {
         $sql = "INSERT INTO candidato (nome, cpf, email, salaprova) VALUES ('$nome', '$cpf', '$email', '$saladeprova')";
         if ($cxn->query($sql) === TRUE) {
             echo "Dados inseridos com sucesso";
         } else {
             echo "Erro ao inserir dados: " . $cxn->error;
         }
-
-
+        $quantcandidatos++;
+        mysqli_query($cxn,"update saladeprova SET quantcandidatos= '$quantcandidatos' WHERE idsala= '$saladeprova' ");
+        
+    }
     $cxn->close();
 }
 ?>
