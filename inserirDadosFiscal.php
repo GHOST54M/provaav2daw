a0ac67932b55fe5bf138886aca614e84835cea58
Marcelo Bartholomeu <83658457+GHOST54M@users.noreply.github.com>
2023-11-28 14:21:29 -0300

    inserirDadosFiscal.php

--- inserirDadosFiscal.php
+++ inserirDadosFiscal.php
@@ -11,14 +11,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $cpf = $_POST['cpf'];
     $saladeprova = $_POST['saladeprova'];
 
-        
+    $selecionar=mysqli_query($cxn, "select * from saladeprova where idsala='$saladeprova'");
+    while ($montar=mysqli_fetch_array($selecionar))
+    {
+        $quantfiscais = $montar['quantfiscais'];
+    }
+    echo "$quantfiscais";
+    if($quantfiscais < 3 )
+    {      
         $sql = "INSERT INTO fiscal (nomefiscal, salaprova, cpf) VALUES ('$nome', '$saladeprova', '$cpf')";
         if ($cxn->query($sql) === TRUE) {
             echo "Dados inseridos com sucesso";
         } else {
             echo "Erro ao inserir dados: " . $cxn->error;
         }
-
+        $quantfiscais++;
+        mysqli_query($cxn,"update saladeprova SET quantfiscais= '$quantfiscais' WHERE idsala= '$saladeprova' ");
+        
+    }
     // Fechar a conexão
     $cxn->close();
 }
